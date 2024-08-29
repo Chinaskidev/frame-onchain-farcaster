1ebeb31bacd46f1951658d80a5022ab614177d2e
Charles <chinaskicharle0@gmail.com>
2024-08-29 11:42:43 -0400

    frame

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const HOST_URL = process.env.NEXT_PUBLIC_HOST_URL;
 
 // Definir el título y la URL de la imagen
 const title = "Amanita Mutation";
-const frameImgUrl = `${HOST_URL}/regenmutation.png`;
+const frameImgUrl = `${HOST_URL}public/regenmutation.png`;
 // Mostrar en la consola el valor de la URL generada para asegurarte de que es válida
 console.log("HOST_URL:", HOST_URL);
 console.log("frameImgUrl:", frameImgUrl);
@@ -16,9 +16,8 @@ export default function Home() {
       <Head> 
         <meta property="og:title" content={title} />
         <meta property="fc:frame" content="vNext" />
-        {/* Asegúrate de que el valor de "fc:frame:image" sea correcto */}
         <meta name="fc:frame:image" content={frameImgUrl} />
-        <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+        <meta name="fc:frame:image:aspect_ratio" content="1:1" />
         <meta property="fc:frame:button:1" content="Mint" />
         <meta property="fc:frame:button:1:action" content="tx" />
         <meta property="fc:frame:button:1:target" content={`${HOST_URL}/api/frame/degen/get-tx-frame`} />
