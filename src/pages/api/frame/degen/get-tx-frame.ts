88a82a24e4ef5d721da0879337c6f116a5342021
Charles <chinaskicharle0@gmail.com>
2024-08-29 13:56:24 -0400

    frame

--- src/pages/api/frame/degen/get-tx-frame.ts
+++ src/pages/api/frame/degen/get-tx-frame.ts
@@ -26,7 +26,7 @@ export default async function handler(
 
      return res.status(200).json({
         ChainId: "eip155:666666666",
-        method: "degen_sendTransaction",
+        method: "eth_sendTransaction",
         params:{
             abi: erc721ContractABI,
             to:  erc721ContractAddress,
