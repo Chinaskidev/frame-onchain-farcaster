7dc885c7dba25ed942c3dcd63412959fc260acfa
Charles <chinaskicharle0@gmail.com>
2024-09-15 17:23:55 -0300

    frame en base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import { FrameMetadata } from '@coinbase/onchainkit/frame';
 const HOST_URL = process.env.NEXT_PUBLIC_HOST_URL;
 
 // Definir el título y la URL de la imagen
-const title = "Amanita";
-const frameImgUrl = `${HOST_URL}/regenmutation.png`;
+const title = "Sivar Volcano";
+const frameImgUrl = `${HOST_URL}sivarletrasvolcano.png`;
 // Mostrar en la consola el valor de la URL generada para asegurarte de que es válida
 console.log("HOST_URL:", HOST_URL);
 console.log("frameImgUrl:", frameImgUrl);
@@ -19,12 +19,13 @@ export default function Home() {
         <meta name="fc:frame" content="vNext" />
         <meta name="fc:frame:image" content={frameImgUrl} />
         <meta name="fc:frame:image:aspect_ratio" content="1:1" />
-        <meta property="fc:frame:button:1" content="Mint" />
-        <meta property="fc:frame:button:1:action" content="tx" />
-        <meta property="fc:frame:button:1:target" content={`${HOST_URL}/api/frame/degen/get-tx-frame`} />
+        <meta property="fc:frame:button:1" content="Start" />
+        <meta property="fc:frame:button:1:action" content="Start" />
+        <meta property="fc:frame:button:2:" content="Mint"/>
+        <meta property="fc:frame:button:2:target" content={`${HOST_URL}/api/frame/base/get-tx-frame`} />
       </Head>
 
-      <h1>Amanita</h1>
+      <h1>Sivar Volcano</h1>
       
     </>
   );
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,11 +1,11 @@
 import { createThirdwebClient, defineChain } from 'thirdweb'; 
-import { degen } from 'thirdweb/chains';
+import { base } from 'thirdweb/chains';
 
 const secretKey = process.env.TW_SECRET_KEY;
 export const CLIENT = createThirdwebClient({
     secretKey: secretKey as string,
 });
-export const CHAIN = defineChain( degen );
+export const CHAIN = defineChain( base );
 
 export const erc721ContractAddress = process.env.CONTRACT_ADDRESS as string;
 
