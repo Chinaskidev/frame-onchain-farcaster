3d36401428175072b44cd0d10570a5f9ed6b8c07
Charles <chinaskicharle0@gmail.com>
2024-09-16 13:33:43 -0300

    frames en base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const HOST_URL = process.env.NEXT_PUBLIC_HOST_URL;
 
 // Definir el título y la URL de la imagen
 const title = "Sivar Volcano";
-const frameImgUrl = `${HOST_URL}sivarletrasvolcano.png`;
+const frameImgUrl = `${HOST_URL}/sivavolcano.png`;
 // Mostrar en la consola el valor de la URL generada para asegurarte de que es válida
 console.log("HOST_URL:", HOST_URL);
 console.log("frameImgUrl:", frameImgUrl);
@@ -15,19 +15,17 @@ export default function Home() {
   return (
     <>
       <Head> 
-        <meta name="og:title" content={title}/>
-        <meta name="fc:frame" content="vNext"/>
-        <meta name="fc:frame:image" content={frameImgUrl}/>
-        <meta name="fc:frame:image:aspect_ratio" content="1:1"/>
-        
-        {/* Primer botón para iniciar */}
-        <meta property="fc:frame:button:1" content="Next" />
-        <meta property="fc:frame:button:1:action" content="post"/>
-        <meta property="fc:frame:button:1:target" content={`${HOST_URL}second-frame`}/>
+        <meta name="og:title" content={title} />
+        <meta name="fc:frame" content="vNext" />
+        <meta name="fc:frame:image" content={frameImgUrl} />
+        <meta name="fc:frame:image:aspect_ratio" content="1:1" />
+        <meta property="fc:frame:button:1" content="Mint" />
+        <meta property="fc:frame:button:1:action" content="tx" />
+        <meta property="fc:frame:button:1:target" content={`${HOST_URL}/api/frame/base/get-tx-frame`} />
       </Head>
 
       <h1>Sivar Volcano</h1>
-      <p>Welcome! Click "Start" and mint a beautiful Volcano</p>
+      
     </>
   );
 }
